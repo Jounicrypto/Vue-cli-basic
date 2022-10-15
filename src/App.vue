80eaf0c087a9f0730c33e26179f97b9680cfa018
Jounicode <96347521+Jounicrypto@users.noreply.github.com>
2022-10-15 20:08:47 +0200

    m

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Houses</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
     <router-view/>
@@ -11,10 +11,8 @@
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  text-decoration: none;
 }
 
 nav {
@@ -22,10 +20,12 @@ nav {
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #000;
+    text-decoration: none;
 
     &.router-link-exact-active {
-      color: #42b983;
+      // color: #42b983;
+      text-decoration: none;
     }
   }
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,57 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="logo.png" alt="logo">
+    <section>
+      <h1>Houses</h1>
+      <button>Create One</button>
+    </section>
+    <section>
+      <template>
+        <img src="ic_search@3x.png" alt="icon-search">
+   <input type="text" v-model="input" placeholder="Search for houses..." />
+  <div class="item fruit" v-for="fruit in fruits" :key="fruit">
+    <!-- <p>{{ fruit }}</p> -->
+  </div>
+  <div class="item error" v-if="input.length < 4">
+     <p>No results found!</p>
+  </div>
+</template>
+      <button>Price</button>
+      <button>Size</button>
+    </section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  data () {
+    return {
+      input: '',
+      fruits: ['apple', 'banana', 'orange'],
+      houses: [
+        {
+
+        }
+      ]
+    }
+      function filteredList () {
+      return fruits.filter((fruit) =>
+    fruit.toLowerCase().includes(input.value.toLowerCase())
+  );
 }
+  }
+
+}
+
 </script>
+
+<style>
+.home{
+  text-align: center;
+  text-decoration: none;
+
+}
+</style>
